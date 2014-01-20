fa0137afb725705baef793a5281eb360c8d2f3ce
Alexandre Cormier <acormier@xnorstudio.com>
2014-01-20 13:16:56 -0500

    Added blank Activity.

---
+++ NFCLogon/src/main/java/com/glideroustigers/nfclogon/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.glideroustigers.nfclogon.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.glideroustigers.nfclogon.R;
+
+public class MainActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.main_activity);
+    }
+}
