5e8fb0639e6fec581ccc7d97780895d7828902ec
Alexandre Cormier <acormier@xnorstudio.com>
2014-01-30 17:19:53 -0500

    Add UDID getter.

--- NFCLogon/src/main/java/com/glideroustigers/nfclogon/activities/MainActivity.java
+++ NFCLogon/src/main/java/com/glideroustigers/nfclogon/activities/MainActivity.java
@@ -2,9 +2,11 @@ package com.glideroustigers.nfclogon.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import com.glideroustigers.nfclogon.R;
+import com.glideroustigers.nfclogon.utils.Device;
 
 public class MainActivity extends Activity
 {
@@ -13,5 +15,6 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.main_activity);
+        Toast.makeText(this, Device.getUUID(this), Toast.LENGTH_LONG).show();
     }
 }
