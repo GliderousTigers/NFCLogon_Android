232c95a3a86481db30faadec1a318d70386286f8
Alex Bergeron <prog.alexbergeron@gmail.com>
2014-01-31 14:51:20 -0500

    NFC Reader now working
    Feature complete: Reading a tag and displaying a toast with the
    containing text, without getting into an activity. Then closing the
    process.

--- NFCLogon/src/main/java/com/glideroustigers/nfclogon/activities/MainActivity.java
+++ NFCLogon/src/main/java/com/glideroustigers/nfclogon/activities/MainActivity.java
@@ -4,12 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.nfc.NfcAdapter;
 import android.content.Intent;
-import android.nfc.NfcAdapter;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.os.Parcelable;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.glideroustigers.nfclogon.R;
@@ -25,43 +25,39 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.main_activity);
-        TextView textView = (TextView) findViewById(R.id.derp);
-        textView.setText("First String");
         resolveIntent(getIntent());
     }
 
     void resolveIntent(Intent intent) {
         // Parse the intent
+       // TextView textView = (TextView) findViewById(R.id.derp);
         String action = intent.getAction();
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-            // When a tag is discovered we send it to the service to be save. We
-            // include a PendingIntent for the service to call back onto. This
-            // will cause this activity to be restarted with onNewIntent(). At
-            // that time we read it from the database and view it.
             Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
             NdefMessage[] msgs;
             if (rawMsgs != null) {
                 msgs = new NdefMessage[rawMsgs.length];
                 for (int i = 0; i < rawMsgs.length; i++) {
                     msgs[i] = (NdefMessage) rawMsgs[i];
+
                 }
+            Toast.makeText(this, new String(msgs[0].getRecords()[0].getPayload()), Toast.LENGTH_SHORT).show();
+            finish();
             } else {
                 // Unknown tag type
                 byte[] empty = new byte[] {};
                 NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
                 NdefMessage msg = new NdefMessage(new NdefRecord[] {record});
                 msgs = new NdefMessage[] {msg};
+                Log.d(TAG, new String(msgs[0].getRecords()[0].getPayload()));
             }
-            TextView textView = (TextView) findViewById(R.id.derp);
-            textView.setText(new String(msgs[0].getRecords()[0].getPayload()));
-        } else {
-            TextView textView = (TextView) findViewById(R.id.derp);
-            textView.setText("Nope");
+            Toast.makeText(this, new String(msgs[0].getRecords()[0].getPayload()), Toast.LENGTH_SHORT).show();
+            finish();
+
+        }
+        else
             return;
         }
     }
-
-
-}
 
 
